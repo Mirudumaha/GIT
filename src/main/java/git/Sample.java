7d460f0dd97ae39fa8484a32854f88b871c4bc6f
Mirudumaha <mirudumaha04@gmail.com>
2023-10-25 17:38:33 +0530

    First task

---
+++ src/main/java/git/Sample.java
@@ -0,0 +1,17 @@
+package git;
+
+public class Sample {
+
+	private void method1() {
+	System.out.println("Tamil");
+
+	}
+	private void method2() {
+	System.out.println("Eng");
+
+	}
+	private void method3() {
+	System.out.println("Hindi");
+
+	}
+}
